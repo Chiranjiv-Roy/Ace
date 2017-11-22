541067c1f7900f5a5bc9e2f7c848d3064e76cc51
Chiranjiv-Roy <chiranjiv.roy@kofax.com>
2017-11-22 16:06:47 +0530

    collaborative editing added

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 $(window).on('load',function(){
   
-  var OldText = "";
+  var last_applied_change = null;
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/javascript");
@@ -10,19 +10,23 @@ $(window).on('load',function(){
   var socket = io.connect();
 
   editor.on('change', function (data) {
-    var NewText = editor.getSession().getValue();
-    var diff = dmp.diff_main(OldText,NewText);
-    dmp.diff_cleanupSemantic(diff);
-    OldText = NewText;
-    socket.emit('diff', diff);
-        console.log(diff);
+    // var NewText = editor.getSession().getValue();
+    // var diff = dmp.diff_main(OldText,NewText);
+    // //dmp.diff_cleanupSemantic(diff);
+    // OldText = NewText;
+    if (last_applied_change != data)
+      socket.emit('diff', JSON.stringify(data));
+        console.log(data);
     });
    
   socket.on('patch', (diff) => {
-    var text = editor.getSession().getValue();
-    var patch = dmp.patch_make(text, diff);
-    var new_text = dmp.patch_apply(patch, text);
-    console.log(new_text);
-    editor.getSession().setValue(new_text[0], { silent: true });
+    // var text = editor.getSession().getValue();
+    // var patch = dmp.patch_make(text, diff);
+    // var new_text = dmp.patch_apply(patch, text);
+    // console.log(new_text);
+    //editor.getSession().setValue(new_text[0], { silent: true });
+    diff = JSON.parse( diff ) ;
+    last_applied_change = diff;
+    editor.getSession().getDocument().applyDeltas( [diff] );
   });
 });
